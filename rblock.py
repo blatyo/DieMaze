64ff2ec80127ad1d0ee98fc77062c624bc4e3b45
Allen Madsen <blatyo@gmail.com>
2010-09-25 13:47:25 -0400

    Created initial code and sample puzzles

---
+++ rblock.py
@@ -0,0 +1,15 @@
+import sys
+
+class Solver:
+	def __init__(self, file_name):
+		self
+	
+	def solve(self):
+		self
+		
+def main():
+	solver = Solver(sys.argv[1])
+	solver.solve()
+
+if __name__ == '__main__':
+	main()
